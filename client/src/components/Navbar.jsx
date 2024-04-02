a1dede332a720daebd3f12138481b02acc53e644
YashwanthMS369 <44166674+YashwanthSiddhuMS@users.noreply.github.com>
2024-04-02 19:08:52 +0530

    Nav bar progress

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React, {userState } from 'react'
+import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined } from '@mui/icons-material';
+import FlexBetween from './FlexBetween';
+import profileImage from 'assets/profile.png';
+import { setMode } from 'state';
+import { UseDispatch } from 'react-redux';
+
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
